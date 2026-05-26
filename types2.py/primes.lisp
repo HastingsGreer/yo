6cda3aadbb25cb9e6ad65fc3abdf2c2e3a142aa0
Hastings Greer <hastings@korionhealth.com>
2026-05-26 14:43:59 -0400

    primetest

--- types2.py/primes.lisp
+++ types2.py/primes.lisp
@@ -6,4 +6,12 @@
 
 (defun main () ((map print_) ((filter isprime) (range 500))))
 
+(defun zero_ (x) 0)
+(defun (consint I64) (n) ((cast Consint) ((map zero_) (range n))))
+(defun (len (List T)) (l) (if l (add 1 (len (cdr l))) 0))
+(defun (I64 Consint) (n) (len ((cast (List I64)) n)))
+(defun (dec Consint) (n) ((cast Consint) (cdr ((Cast List) n))))
+(defun (sub Consint Consint) (a b) (if b (sub (dec a) (dec b)) a))
+
+(defun main () (print_ (I64 (consint 9))))
 
