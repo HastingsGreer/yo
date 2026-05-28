de20118dc65ca409bbc700c943072e1817dde00d
Hastings <deet@2>
2026-05-28 14:50:05 +0000

    move to prelude

--- types2.py/broadcast.lisp
+++ types2.py/broadcast.lisp
@@ -9,27 +9,3 @@
 (defun main () (print (circ (range 10))))
 
 
-(defun ((broadcast F) A B) (a b) (F a b))
-
-(defun ((broadcast F) (List A) B) (a b) (if a 
-					    (cons (F (car a) b) ((broadcast F) (cdr a) b))
-					    (nil (infer (F ((nilptr A)) b)))))
-
-(defun ((broadcast F) A (List B)) (a b) (if b 
-					    (cons (F a (car b)) ((broadcast F) a (cdr b)))
-					    (nil (infer (F a (car b))))))
-
-(defun ((broadcast F) (List A) (List B)) (a b)
-	  (cons 
-	    ((broadcast F) (car a) (car b)) 
-	    (if (cdr a)
-	        (if (cdr b) 
-		    ((broadcast F) (cdr a) (cdr b) )
-		    ((broadcast F) (cdr a) b))
-		(if (cdr b)
-		    ((broadcast F) a (cdr b))
-		    (nil (infer ((broadcast F) (car a) (car b))))))))
-
-
-
-
--- types2.py/prelude.lisp
+++ types2.py/prelude.lisp
@@ -48,6 +48,30 @@
 	  (cons (car l) ((filter F) (cdr l))) 
 	  ((filter F) (cdr l))) 
       ((nilptr (List T)))))
+(defun ((broadcast F) A B) (a b) (F a b))
+
+(defun ((broadcast F) (List A) B) (a b) (if a 
+					    (cons (F (car a) b) ((broadcast F) (cdr a) b))
+					    (nil (infer (F ((nilptr A)) b)))))
+
+(defun ((broadcast F) A (List B)) (a b) (if b 
+					    (cons (F a (car b)) ((broadcast F) a (cdr b)))
+					    (nil (infer (F a (car b))))))
+
+(defun ((broadcast F) (List A) (List B)) (a b)
+	  (cons 
+	    ((broadcast F) (car a) (car b)) 
+	    (if (cdr a)
+	        (if (cdr b) 
+		    ((broadcast F) (cdr a) (cdr b) )
+		    ((broadcast F) (cdr a) b))
+		(if (cdr b)
+		    ((broadcast F) a (cdr b))
+		    (nil (infer ((broadcast F) (car a) (car b))))))))
+
+
+
+
 
 (defun (add I64 I64) (a b) (sub a (sub 0 b)))
 (defun (mul T T) (a b) 
