3fcb396352582a44f113773918f16ea3fb8a6f5d
hastings <hastings.greer@gmail.com>
2026-06-01 20:56:17 -0400

    wewlad

--- parse.py
+++ parse.py
@@ -10,12 +10,15 @@ def parse(program_file):
     program_file = program_file.split("\n")
     program_file = "\n".join([p.split("//")[0] for p in program_file])
     program_file = program_file.split('"')
-    for j in range(1, len(program_file), 2):
+    for j in range( len(program_file)):
+      if j% 2:
         program_file[j] = eval('"' + program_file[j] + '"')
         program_file[j] = "((cast String) " + cons_of_string(program_file[j]) + ")"
+      else:
+        program_file[j] = program_file[j].replace( "\\", "\\\\")
     program_file = "".join(program_file)
 
-    program_file = re.sub(r"([λ/@*{}\.\,:'\"<>\|A-Za-z+_=\-\?0-9\$]+)", r"'\1',", program_file)
+    program_file = re.sub(r"([λ\\/@*{}\.\,:'\"<>\|A-Za-z+_=\-\?0-9\$]+)", r"'\1',", program_file)
     program_file = "(" + re.sub("\\)", "),", program_file) + ")"
     return eval(program_file)
 
